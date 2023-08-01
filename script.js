f1edd9fc98b2fd164943c1517836b5c8f790dbe8
Roman <105315693+mokis3k@users.noreply.github.com>
2023-08-01 23:12:42 +0300

    correcting mistakes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Name
 userName = prompt(`Enter name:`).trim();
 if (userName === null || userName.length == 0) {
     userName = `Your name`;
 }
 
+// Surname
 userSurname = prompt(`Enter surname:`).trim();
 if (userSurname === null || userSurname.length == 0) {
     userSurname = `Your surname`;
 }
 
+// Email
 userEmail = prompt(`Enter email:`);
 if (userEmail === null || userEmail.length == 0) {
     userEmail = `Email not defined`;
@@ -25,16 +28,15 @@ if (userEmail.includes(`@`) != true) {
     userEmail = `not valid email <b>${userEmail}</b> (symbol @ find in first place, symbol @ find in last place)`;
 }
 
+// Year of Birth
 userBirth = prompt(`Enter birth year:`).replaceAll(` `, ``)
-if (userBirth === null || userBirth.length == 0) {
-    userBirth = `Your birth year`;
+let userAge;
+if (userBirth === null || userBirth.length == 0 || isNaN(+(userBirth))) {
+    userAge = `Your age`;
+} else {
+    actualYear = new Date().getFullYear();
+    userAge = actualYear - +(userBirth);
 }
 
-actualYear = new Date().getFullYear();
-userAge = actualYear - +(userBirth);
-
-if (isNaN(userAge)) {
-    userAge = `Your age`
-}
-
+// Output
 document.write(`Full name: ${userName + ` ` + userSurname}<br>Email: ${userEmail}<br>Age: ${userAge}`);
